03d13326846da297873bf042d7e6e471012ccc32
Rinto Jose <rintoj@gmail.com>
2023-08-03 21:37:07 +0530

    fix: export

--- src/util/to-array-by-property.ts
+++ src/util/to-array-by-property.ts
@@ -1,11 +1,4 @@
-import { AllRequired, ById } from '../types'
-
-export function toByProperty<T>(array: T[], property: keyof AllRequired<T> = 'id' as any) {
-  return array?.reduce((a, value) => {
-    a[value[property] as any] = value
-    return a
-  }, {} as ById<T>)
-}
+import { ById } from '../types'
 
 export function toArrayByProperty<T extends Record<string, any>>(
   fields: T[],
